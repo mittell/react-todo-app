b15a6242aa5c05d70ab2a38f7533af93a4af59ee
Chris Mittell <mittell@hotmail.co.uk>
2022-08-27 10:57:49 +0900

    feat: added base todo logic

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
-function Todo({ todo }) {
+function Todo({ todo, todos, setTodos }) {
+	const handleDelete = () => {
+		setTodos(todos.filter((el) => el.id !== todo.id));
+	};
+
+	const handleComplete = () => {
+		setTodos(
+			todos.map((item) => {
+				if (item.id === todo.id) {
+					return {
+						...item,
+						completed: !item.completed,
+					};
+				}
+
+				return item;
+			})
+		);
+	};
+
 	return (
 		<div className='todo'>
-			<li className='todo-item'>{todo.text}</li>
-			<button className='complete-btn'>
+			<li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+				{todo.text}
+			</li>
+			<button className='complete-btn' onClick={handleComplete}>
 				<i className='fas fa-check'></i>
 			</button>
-			<button className='trash-btn'>
+			<button className='trash-btn' onClick={handleDelete}>
 				<i className='fas fa-trash'></i>
 			</button>
 		</div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Todo from './Todo';
 
-function TodoList({ todos }) {
+function TodoList({ todos, setTodos }) {
 	return (
 		<div className='todo-container'>
 			<ul className='todo-list'>
 				{todos.map((todo) => (
-					<Todo todo={todo} key={todo.id} />
+					<Todo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
 				))}
 			</ul>
 		</div>
